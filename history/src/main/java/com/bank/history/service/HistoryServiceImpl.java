3702bef943995b4ec2737049d3ecf6bc85ea76f0
Misha <misha-almazov@mail.ru>
2025-03-13 11:39:04 +0300

    Подправил code style

--- history/src/main/java/com/bank/history/service/HistoryServiceImpl.java
+++ history/src/main/java/com/bank/history/service/HistoryServiceImpl.java
@@ -1,11 +1,8 @@
 package com.bank.history.service;
 
-import com.bank.history.dto.HistoryDto;
 import com.bank.history.entity.History;
-import com.bank.history.mapper.HistoryMapper;
 import com.bank.history.repository.HistoryRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,15 +13,9 @@ import java.util.List;
 public class HistoryServiceImpl implements HistoryService {
 
     private final HistoryRepository historyRepository;
-    private final HistoryMapper historyMapper;
-    private final KafkaTemplate<String, HistoryDto> kafkaTemplate;
 
-    public HistoryServiceImpl(HistoryRepository historyRepository,
-                              HistoryMapper historyMapper,
-                              KafkaTemplate<String, HistoryDto> kafkaTemplate) {
+    public HistoryServiceImpl(HistoryRepository historyRepository) {
         this.historyRepository = historyRepository;
-        this.historyMapper = historyMapper;
-        this.kafkaTemplate = kafkaTemplate;
     }
 
     @Override
