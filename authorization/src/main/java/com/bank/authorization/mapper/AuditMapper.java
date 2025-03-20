da794579217b3567fa98ad3c12e5c9474c08b714
Valeri Eremeev <valeri-eremeev@yandex.ru>
2025-03-20 11:15:36 +0300

    Удалил неиспользуемый импорт из AuditMapper.

--- authorization/src/main/java/com/bank/authorization/mapper/AuditMapper.java
+++ authorization/src/main/java/com/bank/authorization/mapper/AuditMapper.java
@@ -3,7 +3,6 @@ package com.bank.authorization.mapper;
 import com.bank.authorization.dto.AuditDto;
 import com.bank.authorization.entity.Audit;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
