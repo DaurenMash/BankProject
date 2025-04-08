c010a7c3416086d433c25a23ab9ad045056c6f6a
Valeri Eremeev <valeri.eremeev@gmail.com>
2025-04-08 14:21:42 +0300

    Новая ветка для Прометеуса и Графаны.

--- authorization/src/main/java/com/bank/authorization/config/SecurityConfig.java
+++ authorization/src/main/java/com/bank/authorization/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -18,5 +20,16 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
