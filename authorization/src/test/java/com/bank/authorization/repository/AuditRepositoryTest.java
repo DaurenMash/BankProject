fcaa504bdf5c609d2aa4c22a03548f207f4568f5
Valeri Eremeev <valeri.eremeev@gmail.com>
2025-04-12 13:11:18 +0300

    Отработал по замечаниям к MR от 11 апреля.

--- authorization/src/test/java/com/bank/authorization/repository/AuditRepositoryTest.java
+++ authorization/src/test/java/com/bank/authorization/repository/AuditRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
@@ -17,6 +20,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.liquibase.enabled=false"
+})
 class AuditRepositoryTest {
 
     @Autowired
